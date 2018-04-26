df735cfc8dc1bb16d4c7c84bf7f7859735fd2fbb
김동현 <gimdonghyeon@gimdonghyeon-ui-MacBook-Pro.local>
2018-04-26 22:57:50 +0900

    PythonAnyWheregit add --all./bin/logstash -f logstash.conf

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p7&y5#a-3r0$tgqudpug%g#i80_#j+*u!yww$(5$=8omdhiqye'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
