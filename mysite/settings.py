f1d733316685c2885d6ca829fb9d3f1783b77996
김동현 <gimdonghyeon@gimdonghyeon-ui-MacBook-Pro.local>
2018-04-28 22:05:05 +0900

    Change my setting file

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p7&y5#a-3r0$tgqudpug%g#i80_#j+*u!yww$(5$=8omdhiqye'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
